dc3c72b54854affa8e58dba184649b4f4e1a8c2c
vagrant <vagrant@scotchbox>
2019-02-26 23:24:29 +0000

    Organised scripts. Testing Google Places API

---
+++ scripts/sidebar.js
@@ -0,0 +1,8 @@
+function sidebar_open() {
+    document.getElementById("sidebar").style.display = "block";
+}
+
+function sidebar_close() {
+    document.getElementById("sidebar").style.display = "none";
+}    
+    
